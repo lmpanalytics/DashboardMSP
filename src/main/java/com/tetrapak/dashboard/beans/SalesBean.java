22e3979a5a3d53cce2ad0ae78fb13b51ab4a6763
lmpanalytics2 <lmpconsulting@hotmail.com>
2016-11-03 12:51:26 +0100

    Add bar chart to index page

--- src/main/java/com/tetrapak/dashboard/beans/SalesBean.java
+++ src/main/java/com/tetrapak/dashboard/beans/SalesBean.java
@@ -14,7 +14,10 @@ import javax.ejb.Stateless;
 import javax.faces.bean.RequestScoped;
 
 import javax.inject.Named;
-import javax.faces.bean.SessionScoped;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 /**
  * This bean makes the sales logic.
@@ -31,6 +34,8 @@ public class SalesBean implements Serializable {
     private List<Invoice> salesList;
     private double totalSales = 0d;
 
+    private BarChartModel model;
+
     /**
      * Creates a new instance of salesBean
      */
@@ -41,6 +46,32 @@ public class SalesBean implements Serializable {
     public void init() {
         this.salesList = makeSales();
         this.totalSales = sumSales();
+
+        model = new BarChartModel();
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 135);
+        girls.set("2008", 120);
+        model.addSeries(boys);
+        model.addSeries(girls);
+        model.setTitle("Bar Chart");
+        model.setLegendPosition("ne");
+        Axis xAxis = model.getAxis(AxisType.X);
+        xAxis.setLabel("Gender");
+        Axis yAxis = model.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
     }
 
     private List<Invoice> makeSales() {
@@ -72,4 +103,7 @@ public class SalesBean implements Serializable {
         this.totalSales = totalSales;
     }
 
+    public BarChartModel getModel() {
+        return model;
+    }
 }
