22402e3207a71b70678392a14490a4ad50444e3c
lmpanalytics2 <lmpconsulting@hotmail.com>
2017-04-13 12:05:41 +0200

    Add Unilever as cust grp

--- src/main/java/com/tetrapak/dashboard/beans/CheckboxViewCustGroup.java
+++ src/main/java/com/tetrapak/dashboard/beans/CheckboxViewCustGroup.java
@@ -51,6 +51,7 @@ public class CheckboxViewCustGroup implements Serializable {
         customerGroups.add("LACTALIS");
         customerGroups.add("NESTLE");
         customerGroups.add("PEPSICO");
+        customerGroups.add("UNILEVER");
 
     }
 
